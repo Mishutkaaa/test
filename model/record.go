30c3afcadecaebbffee89cd2435cc9dc86557868
Mishutka <zzMishutkazz@yandex.ru>
2026-01-12 13:23:29 +0500

    update model

--- model/record.go
+++ model/record.go
@@ -3,12 +3,12 @@ package model
 import (
 	"time"
 
-	"github.com/google/uuid"
+	"github.com/jackc/pgx/pgtype"
 )
 
 type Record struct {
-	ServiceName string    `json:"service_name,omitempty"`
-	Price       int       `json:"price,omitempty"`
-	UserID      uuid.UUID `json:"user_id,omitempty"`
-	StartDate   time.Time `json:"start_date,omitempty"`
+	ServiceName string      `json:"service_name,omitempty"`
+	Price       int         `json:"price,omitempty"`
+	UserID      pgtype.UUID `json:"user_id,omitempty"`
+	StartDate   time.Time   `json:"start_date,omitempty"`
 }
