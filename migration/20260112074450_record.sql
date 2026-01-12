5509a9a45d415ffb0a81493c387e0d16334cba59
Mishutka <zzMishutkazz@yandex.ru>
2026-01-12 15:34:18 +0500

    update model and migration

--- migration/20260112074450_record.sql
+++ migration/20260112074450_record.sql
@@ -1,11 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 Create table record(
-    id uuid not null,
+    id serial not null,
+    user_id uuid not null,
     service_name text not null,
     price int not null,
     start_date date not null,
-    primary key(id)
+    primary key(user_id)
 );
 -- +goose StatementEnd
 
--- model/record.go
+++ model/record.go
@@ -7,6 +7,7 @@ import (
 )
 
 type Record struct {
+	ID          int         `json:"id,omitempty"`
 	ServiceName string      `json:"service_name,omitempty"`
 	Price       int         `json:"price,omitempty"`
 	UserID      pgtype.UUID `json:"user_id,omitempty"`
