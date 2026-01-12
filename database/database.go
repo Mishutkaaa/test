e0e16d3279d3bc5e45b5fe9dccca723add505876
Mishutka <zzMishutkazz@yandex.ru>
2026-01-12 13:50:13 +0500

    database connection

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"test/config"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Conn() *sql.DB {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println(".env not found")
+	}
+	conf := config.NewConfigFromEnv()
+	conn := conf.Config()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		log.Println("err conn db", err)
+		return nil
+	}
+	return db
+}
