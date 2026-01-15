676a9f46efe04bc38673019a644a8dcdf8dce1aa
Mishutka <zzMishutkazz@yandex.ru>
2026-01-15 14:55:17 +0500

    update handles

--- handles/record.go
+++ handles/record.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net/http"
 	"test/model"
-
-	"github.com/jackc/pgx/pgtype"
 )
 
 func GetByID(db *sql.DB) http.HandlerFunc {
@@ -17,12 +15,14 @@ func GetByID(db *sql.DB) http.HandlerFunc {
 
 		if r.Method != "GET" {
 			http.Error(w, "method not allowed ", http.StatusMethodNotAllowed)
+			return
 		}
 
 		id := r.PathValue("id")
 		row := db.QueryRow("select user_id, service_name, price, to_char(start_date, 'MM-YYYY') AS start_date from record where id = $1", id)
 		if err := row.Scan(&record.UserID, &record.ServiceName, &record.Price, &record.StartDate); err != nil {
-			log.Println("cannot scan row", err)
+			http.Error(w, "cannot scan row", http.StatusInternalServerError)
+			log.Println(err)
 		}
 		if record == (model.Record{}) {
 			http.Error(w, "record not found", http.StatusNotFound)
@@ -38,18 +38,21 @@ func GetAll(db *sql.DB) http.HandlerFunc {
 
 		if r.Method != "GET" {
 			http.Error(w, "method not allowed ", http.StatusMethodNotAllowed)
+			return
 		}
 
 		rows, err := db.Query("select user_id, service_name, price, to_char(start_date, 'MM-YYYY') AS start_date from record")
 		if err != nil {
-			log.Println("cannot get rows", err)
+			http.Error(w, "cannot get rows", http.StatusInternalServerError)
+			log.Println(err)
 			return
 		}
 		defer rows.Close()
 		for rows.Next() {
 			record := model.Record{}
 			if err := rows.Scan(&record.UserID, &record.ServiceName, &record.Price, &record.StartDate); err != nil {
-				log.Println("cannot scan row", err)
+				http.Error(w, "cannot scan rows", http.StatusInternalServerError)
+				log.Println(err)
 			}
 			records = append(records, record)
 		}
@@ -64,6 +67,7 @@ func CreateRecord(db *sql.DB) http.HandlerFunc {
 
 		if r.Method != "POST" {
 			http.Error(w, "method not allowed ", http.StatusMethodNotAllowed)
+			return
 		}
 
 		if err := json.NewDecoder(r.Body).Decode(&record); err != nil {
@@ -72,13 +76,16 @@ func CreateRecord(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		if record.UserID == (pgtype.UUID{}) || record.ServiceName == "" || record.Price == 0 || record.StartDate == "" {
+		if record.UserID == "" || record.ServiceName == "" || record.Price == 0 || record.StartDate == "" {
 			http.Error(w, "please fill in all required fields", http.StatusBadRequest)
 			return
 		}
 
-		if _, err := db.Exec("insert into record (user_id, service_name, price, start_date) values($1, $2, $3, $4)", record.UserID, record.ServiceName, record.Price, record.StartDate); err != nil {
-			log.Println("cannot create record", record)
+		record.StartDate = "01-" + record.StartDate
+
+		if _, err := db.Exec("insert into record (user_id, service_name, price,  start_date) values($1, $2, $3, $4)", record.UserID, record.ServiceName, record.Price, record.StartDate); err != nil {
+			http.Error(w, "cannot create record", http.StatusInternalServerError)
+			log.Println(err)
 			return
 		}
 
@@ -90,12 +97,14 @@ func DeleteRecord(db *sql.DB) http.HandlerFunc {
 
 		if r.Method != "DELETE" {
 			http.Error(w, "method not allowed ", http.StatusMethodNotAllowed)
+			return
 		}
 
 		id := r.PathValue("id")
 
 		if _, err := db.Exec("delete from record where id = $1", id); err != nil {
-			log.Println("cannot delete record", err)
+			http.Error(w, "cannot delete record", http.StatusInternalServerError)
+			log.Println(err)
 			return
 		}
 	}
@@ -105,6 +114,7 @@ func GetSumm(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			http.Error(w, "method not allowed ", http.StatusMethodNotAllowed)
+			return
 		}
 
 		dateTo := r.URL.Query().Get("dateTo")
@@ -127,7 +137,7 @@ func GetSumm(db *sql.DB) http.HandlerFunc {
 		if serviceName != "" {
 			query += fmt.Sprintf("and service_name = '%s' ", serviceName)
 		}
-		
+
 		row := db.QueryRow(query)
 		if err := row.Scan(&record.Price); err != nil {
 			log.Println("cannot scan row", err)
